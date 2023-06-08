f694bd3adc7398332306f15d3c1053b83df8736e
khpolash <polash250@gmail.com>
2023-06-08 15:38:14 +0600

    add classRoomList component

---
+++ src/app/modules/admin/classRoom/class-room-list/class-room-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-room-list',
+  templateUrl: './class-room-list.component.html',
+  styleUrls: ['./class-room-list.component.scss']
+})
+export class ClassRoomListComponent {
+
+}
