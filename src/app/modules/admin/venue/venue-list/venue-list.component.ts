029bb110b661f0d4435c861b4d2f60477c7d2b64
khpolash <polash250@gmail.com>
2023-06-08 15:39:25 +0600

    add venueList component

---
+++ src/app/modules/admin/venue/venue-list/venue-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venue-list',
+  templateUrl: './venue-list.component.html',
+  styleUrls: ['./venue-list.component.scss']
+})
+export class VenueListComponent {
+
+}
