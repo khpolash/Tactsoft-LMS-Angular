9adc3a49b444c8f6c75d1de18d15bb9aa1618256
khpolash <polash250@gmail.com>
2023-06-12 11:42:37 +0600

    Add properties on Trainer Model

--- src/app/features/models/trainer.model.ts
+++ src/app/features/models/trainer.model.ts
@@ -1,2 +1,18 @@
 export interface Trainer {
+  id:number;
+  name:string;
+  dateOfBirth:Date;
+  joiningDate:Date;
+  address:string;
+  phone:string;
+  email:string;
+  academicInfo:string;
+  experience:string;
+  expertise:string;
+  about:string;
+  facebookLink:string;
+  twitterLink:string;
+  linkedinLink:string;
+  curriculumVitae:string;
+  picture:string;
 }
