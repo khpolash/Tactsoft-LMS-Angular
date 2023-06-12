8b92d5e7e470fdd5c97ffbacf004d18cd3251a05
khpolash <polash250@gmail.com>
2023-06-12 11:52:16 +0600

    Add ClassRoom List

--- src/app/modules/admin/classRoom/class-room-list/class-room-list.component.ts
+++ src/app/modules/admin/classRoom/class-room-list/class-room-list.component.ts
@@ -1,10 +1,141 @@
-import { Component } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subject, pipe, share, debounceTime, distinctUntilChanged, startWith, switchMap, map, merge } from 'rxjs';
+import { MessageService } from 'src/app/core/services/message/message.service';
+import { Messages } from 'src/app/core/services/message/messages';
+import { RouteService } from 'src/app/core/services/routes/route.service';
+import { FrontRoute } from 'src/app/features/configs/front-route';
+import { ClassRoom } from 'src/app/features/models/class-room.model';
+import { ClassRoomService } from 'src/app/features/services/class-room.service';
 
 @Component({
   selector: 'app-class-room-list',
   templateUrl: './class-room-list.component.html',
   styleUrls: ['./class-room-list.component.scss']
 })
-export class ClassRoomListComponent {
+export class ClassRoomListComponent implements OnInit {
+
+  frontRoute = FrontRoute;
+
+  classRoom: ClassRoom[];
+  cities: ClassRoom[];
+
+  displayedColumns: string[] = ["position","name","number","action"];
+  dataSource: MatTableDataSource<ClassRoom> = new MatTableDataSource();
+  pageSizeOptions: number[] = [10, 25, 50, 100];
+  pageIndex = 0;
+  pageSize = 10;
+  length = 0;
+
+  selection = new SelectionModel<ClassRoom>(true, []);
+  filterInput: Subject<string> = new Subject<string>();
+  filterValue: string = "";
+
+  isLoadingResults = true;
+  message = Messages;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+
+  constructor(
+    public dialog: MatDialog,
+    public classRoomService: ClassRoomService,
+    public routeService: RouteService,
+    private messageService: MessageService) { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void {
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.getData();
+  }
+
+  customPipe = pipe(
+    share(), debounceTime(1000), distinctUntilChanged(), startWith({}),
+    switchMap(() => {
+      this.isLoadingResults = true;
+      return this.load();
+    }), map((data: any) => {
+      this.isLoadingResults = false;
+      if (data === null) {
+        return [];
+      }
+      return data;
+    })
+  );
+
+  customSubscribe = (data: any) => {
+    this.classRoom = data.data.data;
+    this.dataSource = new MatTableDataSource(data.data.data);
+    this.paginator.pageIndex = data.data.pageIndex;
+    this.paginator.length = data.data.total;
+  };
+
+  load(): any {
+    return this.classRoomService.getSearch(
+      this.paginator.pageIndex,
+      this.paginator.pageSize,
+      this.sort.active,
+      this.sort.direction,
+      this.filterValue
+    );
+  }
+
+  getData(): void {
+    merge(this.sort.sortChange, this.paginator.page, this.filterInput)
+      .pipe(this.customPipe)
+      .subscribe((data: any) => {
+        this.customSubscribe(data);
+      });
+  }
+
+  reload(reloadType: boolean): void {
+    if (reloadType) {
+      this.paginator.pageIndex = 0;
+      this.filterValue = "";
+    }
+
+    this.getData();
+  }
+  pageChanged(event: PageEvent) {
+    this.paginator.pageSize = event.pageSize;
+    this.paginator.pageIndex = event.pageIndex;
+  }
+
+  search(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+
+    if (this.filterInput.observers.length === 0) {
+      this.filterInput.pipe(debounceTime(1000), distinctUntilChanged());
+    }
+
+    this.filterInput.next(value);
+    this.filterInput.subscribe((e) => (this.filterValue = e));
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  remove(classRoomId: number): void {
+    if (classRoomId) {
+      this.messageService.confirm().then((result) => {
+        if (result.value) {
+          this.classRoomService.delete(classRoomId).subscribe((e) => {
+            this.messageService.success(this.message.deleteSuccess);
+            this.reload(true);
+          });
+        }
+      });
+    }
+  }
+
 
 }
